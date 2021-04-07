fb8e8403a129d7502f5a666f0463deb89d27f9d3
Kendyl Hull <hullkendyl@gmail.com>
2021-04-07 08:58:52 -0400

    practice .read()

--- files_3.py
+++ files_3.py
@@ -1,9 +1,17 @@
 fhand = open('textFile.txt')
+#count
 count = 0
 for line in fhand:
     count = count + 1
 print('Line Count: ', count)
 
-mylist = ["apple", "orange", "cherry"]
+#read
+fhand = open('textFile.txt')
+print(fhand.read())
+
+#len() returns the number of items in an object or number of characters in
+# a string
+mylist = ["apple", "orange", "cherry", "pear"]
 x = len(mylist)
-print(x)
+print('Total Number of Fruit: ', x)
+
